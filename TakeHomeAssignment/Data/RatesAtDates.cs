7ad57f1dc4fa84b3a7297b3c46dc43c685435bbb
Tony Berglund <tony_berglund@outlook.com>
2021-10-09 03:28:03 +0200

    RatesAtDates

---
+++ TakeHomeAssignment/Data/RatesAtDates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeHomeAssignment.Data
+{
+    public class RatesAtDates
+    {
+        public string Date { get; set; }
+        public string Currency { get; set; }
+        public double Rate { get; set; }
+      
+    }
+}
